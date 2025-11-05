82c46481c8a552078a0ac1a4e26a4d4c6ed013c4
masriadiNotDev <adiganzxd@gmail.com>
2025-11-05 16:30:49 +0800

    Update segitiga.py

--- segitiga.py
+++ segitiga.py
@@ -5,4 +5,4 @@
 alas_segitiga = int(input("masukan alas segitiga :"))
 tinggi_segitiga = int(input("masukan tinggi segitiga : "))
 luas = alas_segitiga * tinggi_segitiga
-print(f"alas : {alas_segitiga} x {tinggi_segitiga}\n= {luas}")
+print(f"alas : {alas_segitiga} x {tinggi_segitiga} : {luas}")
